f724c0c2d83c2315ab14d83cb5412c4e461887ac
rapolurenu <159129847+rapolurenu@users.noreply.github.com>
2024-05-28 15:35:16 +0530

    Update reducer.jsx

--- Redux/src/components/reducer.jsx
+++ Redux/src/components/reducer.jsx
@@ -1,23 +1,22 @@
-// src/reducer.js
 const initialState = {
-    likeCount: 0
+  likeCount: 0
 };
 
 const reducer = (state = initialState, action) => {
-    switch(action.type) {
-        case 'LIKE':
-            return {
-                ...state,
-                likeCount: state.likeCount + 1
-            };
-        case 'UNLIKE':
-            return {
-                ...state,
-                likeCount: state.likeCount - 1
-            };
-        default:
-            return state;
-    }
+  switch(action.type) {
+    case 'LIKE':
+      return {
+        ...state,
+        likeCount: state.likeCount + 1
+      };
+    case 'UNLIKE':
+      return {
+        ...state,
+        likeCount: state.likeCount - 1
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
