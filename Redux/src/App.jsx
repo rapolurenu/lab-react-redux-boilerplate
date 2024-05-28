efbe1d36c4fc25221e735a092fa5fcbd7ad39400
rapolurenu <159129847+rapolurenu@users.noreply.github.com>
2024-05-28 15:35:37 +0530

    Update App.jsx

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -1,14 +1,12 @@
-// src/App.js
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import LikeCounter from './components/LikeCounter';
 
 const App = () => {
-    return (
-        <div className="App">
-            <LikeCounter />
-        </div>
-    );
+  return (
+    <div className="App">
+      <LikeCounter />
+    </div>
+  );
 };
 
 export default App;
